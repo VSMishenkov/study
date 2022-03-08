d04a11be762bbb8c83b40ed0f6d67f5c11da6479
Vladislav <vsmishenkov@gmail.com>
2022-03-09 00:39:11 +0300

    Create chess2.cpp

---
+++ chess2/chess2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((a - c)*(a-c) <= 1 && (b-d)*(b - d) <= 1) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
